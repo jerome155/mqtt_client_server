8fcbe6f7b9358a3f4a606e347440d45d51cb70ab
Jerome Oesch <jerome.oesch@buhlergroup.com>
2018-12-05 14:16:37 +0100

    Initial commit

---
+++ client/MqttClientTest/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Exceptions;
+using MQTTnet.Extensions.ManagedClient;
+using MQTTnet.Protocol;
+using MQTTnet.Serializer;
+
+namespace MqttClientTest
+{
+    static class Program
+    {
+        private static readonly IMqttClient MqttClient = new MqttFactory().CreateMqttClient();
+
+        private static void Main(string[] args)
+        {
+            ConnectClient();
+            AttachEventDelegates();
+            RunClient();
+            ShutdownClient();
+        }
+
+        private static void AttachEventDelegates()
+        {
+            MqttClient.ApplicationMessageReceived += MqttClientOnApplicationMessageReceived;
+            MqttClient.Connected += MqttClientOnConnected;
+            MqttClient.Disconnected += MqttClientOnDisconnected;
+        }
+
+        
+
+    private static async void MqttClientOnDisconnected(object sender, MqttClientDisconnectedEventArgs e) 
+        {
+            if (e.Exception == null)
+            {
+                Console.WriteLine("Client disconnected.");
+            }
+            else
+            {
+                Console.WriteLine("Client disconnected: " + e.Exception.Message);
+            }
+            
+            Console.WriteLine("Retrying in 5...");
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            ConnectClient();
+        }
+
+        private static void MqttClientOnConnected(object sender, MqttClientConnectedEventArgs e)
+        {
+            Console.WriteLine("Client connected: " + e.IsSessionPresent);
+            //SubscribeToTopic();
+        }
+
+        private static void ShutdownClient()
+        {
+            UnsubscribeClient();
+            DisconnectClient();
+        }
+
+        private static async void UnsubscribeClient()
+        {
+            await MqttClient.UnsubscribeAsync(new List<string> { "MainTopic" });
+            Console.WriteLine("Unsubscribed Client.");
+        }
+
+        private static async void DisconnectClient()
+        {
+            await MqttClient.DisconnectAsync();
+            Console.WriteLine("Disconnected Client.");
+        }
+
+        private static void RunClient()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "Subscribe":
+                        SubscribeToTopic();
+                        break;
+                    case "Unsubscribe":
+                        UnsubscribeClient();
+                        break;
+                    case "Connect":
+                        ConnectClient();
+                        break;
+                    case "Disconnect":
+                        DisconnectClient();
+                        break;
+                    case "Shutdown":
+                        return;
+                }
+            }
+
+        }
+
+        private static void SubscribeToTopic()
+        {
+            MqttClient.SubscribeAsync(new List<TopicFilter> { new TopicFilterBuilder().WithTopic("MainTopic").WithExactlyOnceQoS().Build()});
+            Console.WriteLine("Subscribed to Topic.");
+        }
+
+        private static async void ConnectClient()
+        {
+            IMqttClientOptions mqttClientOptions = new MqttClientOptionsBuilder()
+                .WithClientId("RealClient")
+                .WithTcpServer("localhost", 1884)
+                .WithKeepAlivePeriod(TimeSpan.FromMilliseconds(-1))
+                .WithProtocolVersion(MqttProtocolVersion.V311)
+                .Build();
+
+            IManagedMqttClientOptions managedMqttClientOptions = new ManagedMqttClientOptionsBuilder()
+                .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+                .WithClientOptions(mqttClientOptions)
+                .Build();
+            
+
+            try
+            {
+                await MqttClient.ConnectAsync(mqttClientOptions);
+            }
+            catch
+            {
+            }
+        }
+
+        private static void MqttClientOnApplicationMessageReceived(object sender, MqttApplicationMessageReceivedEventArgs e)
+        {
+            Console.WriteLine("Message received: " + Encoding.UTF8.GetString(e.ApplicationMessage.Payload));
+        }
+    }
+}
---
+++ server/MqttServerTest/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Diagnostics;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+
+namespace MqttServerTest
+{
+    static class Program
+    {
+        private static IMqttServer _mqttServer;
+        //private static IMqttServerOptions _mqttServerOptions;
+        //private static readonly IMqttClient MqttClient = new MqttFactory().CreateMqttClient();
+        private static RetainedMessageHandler _retainedMsgHandler = new RetainedMessageHandler();
+        private const string MqttTopic = "MainTopic";
+
+        private static void Main(string[] args)
+        {
+            StartServer();
+            //ConnectSendingClient();
+            AttachAlarmDelegates();
+            RunServer();
+            ShutdownServer();
+        }
+
+        private static void AttachAlarmDelegates()
+        {
+            _mqttServer.ClientConnected += MqttServerOnClientConnected;
+            _mqttServer.ClientDisconnected += MqttServerOnClientDisconnected;
+            _mqttServer.ClientSubscribedTopic += MqttServerOnClientSubscribedTopic;
+            _mqttServer.ClientUnsubscribedTopic += MqttServerOnClientUnsubscribedTopic;
+        }
+
+        private static void MqttServerOnClientUnsubscribedTopic(object sender, MQTTnet.Server.MqttClientUnsubscribedTopicEventArgs e)
+        {
+            Console.WriteLine("Client unsubscribed from topic: " + e.ClientId + ", " + e.TopicFilter);
+        }
+
+
+        private static void MqttServerOnClientDisconnected(object sender, MQTTnet.Server.MqttClientDisconnectedEventArgs e)
+        {
+            Console.WriteLine("Client disconnected: " + e.ClientId + ", Gracefull shutdown? " + e.WasCleanDisconnect);
+        }
+
+        //private static async void ConnectSendingClient()
+        //{
+        //    IMqttClientOptions mqttClientOptions = new MqttClientOptionsBuilder()
+        //        .WithClientId("MyClient")
+        //        .WithTcpServer("localhost", 1884)
+        //        .WithKeepAlivePeriod(TimeSpan.FromMilliseconds(-1))
+        //        .Build();
+
+        //    await MqttClient.ConnectAsync(mqttClientOptions);
+        //}
+
+        private static async void ShutdownServer()
+        {
+            try
+            {
+                await _mqttServer.ClearRetainedMessagesAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            
+            await _mqttServer.StopAsync();
+        }
+
+        private static async void StartServer()
+        {
+            if (_mqttServer == null) _mqttServer = new MqttFactory().CreateMqttServer();
+
+            //_mqttServerOptions = new MqttServerOptionsBuilder()
+            //    .WithMaxPendingMessagesPerClient(100)
+            //    .WithConnectionBacklog(100)
+            //    .WithDefaultEndpointPort(1884)
+            //    .WithConnectionValidator(c => c.ReturnCode = MqttConnectReturnCode.ConnectionAccepted)
+            //    .WithStorage(_retainedMsgHandler)
+            //    .WithPersistentSessions()
+            //    .Build();
+
+            MqttServerOptions opt = new MqttServerOptions();
+            opt.PendingMessagesOverflowStrategy = MqttPendingMessagesOverflowStrategy.DropOldestQueuedMessage;
+            opt.EnablePersistentSessions = true;
+            opt.MaxPendingMessagesPerClient = 1000;
+            opt.DefaultEndpointOptions.Port = 1884;
+            opt.DefaultEndpointOptions.IsEnabled = true;
+            opt.ConnectionValidator = c => c.ReturnCode = MqttConnectReturnCode.ConnectionAccepted;
+            opt.Storage = _retainedMsgHandler;
+
+            await _mqttServer.StartAsync(opt);
+
+            Console.WriteLine("Server Pending Messages Overflow Strategy: " + _mqttServer.Options.PendingMessagesOverflowStrategy.ToString());
+            Console.WriteLine("Server Persistent Sessions: " + _mqttServer.Options.EnablePersistentSessions);
+
+            Console.WriteLine("Server started.");
+        }
+
+        private static void MqttServerOnClientSubscribedTopic(object sender, MQTTnet.Server.MqttClientSubscribedTopicEventArgs e)
+        {
+            Console.WriteLine("Client subscribed to topic: " + e.ClientId + ", " + e.TopicFilter);
+        }
+
+        private static void MqttServerOnClientConnected(object sender, MQTTnet.Server.MqttClientConnectedEventArgs e)
+        {
+            Console.WriteLine("Client connected: " + e.ClientId);
+        }
+
+
+        private static void RunServer()
+        {
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "SendMessage":
+                        SendMessage();
+                        break;
+                    case "Shutdown":
+                        return;
+                }
+            }
+            
+        }
+
+        private static int _msgCounter = 0;
+
+        private static async void SendMessage()
+        {
+            try
+            {
+                _msgCounter++;
+                await _mqttServer.PublishAsync(new MqttApplicationMessageBuilder()
+                    .WithTopic(MqttTopic)
+                    .WithRetainFlag()
+                    .WithPayload(DateTime.Now + ": TestMessage "  + _msgCounter)
+                    .WithExactlyOnceQoS()
+                    .WithQualityOfServiceLevel(MqttQualityOfServiceLevel.ExactlyOnce)
+                    .Build());
+                
+                Console.WriteLine("Message sent (Counter = " + _msgCounter + ")");
+
+                Console.WriteLine(_mqttServer.GetClientSessionsStatusAsync().Result.Count);
+
+                foreach (IMqttClientSessionStatus status in _mqttServer.GetClientSessionsStatusAsync().Result)
+                {
+                    
+                    Console.WriteLine("Current retained message count per Client: " + status.ClientId + ", " + status.PendingApplicationMessagesCount);                       
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}

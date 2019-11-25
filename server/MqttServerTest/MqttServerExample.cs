0a20fd9aa67cb05f7da7d8f8cbfb2d46ab3db9ed
Jerome Oesch <jerome.oesch@buhlergroup.com>
2019-11-25 13:40:36 +0100

    Added example for Mario Arlitt.

---
+++ server/MqttServerTest/MqttServerExample.cs
@@ -0,0 +1,38 @@
+﻿using MQTTnet;
+using MQTTnet.Protocol;
+using MQTTnet.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MqttServerTest
+{
+    public class MqttServerExample
+    {
+        private IMqttServer _mqttServer;
+
+        public MqttServerExample()
+        {
+            if (_mqttServer == null) _mqttServer = new MqttFactory().CreateMqttServer();
+
+            MqttServerOptions opt = new MqttServerOptions();
+            opt.PendingMessagesOverflowStrategy = MqttPendingMessagesOverflowStrategy.DropOldestQueuedMessage;
+            opt.EnablePersistentSessions = true;
+            opt.MaxPendingMessagesPerClient = 1000;
+            opt.DefaultEndpointOptions.Port = 1883;
+            opt.DefaultEndpointOptions.IsEnabled = true;
+            opt.ConnectionValidator = c => c.ReturnCode = MqttConnectReturnCode.ConnectionAccepted;
+
+            _mqttServer.StartAsync(opt);
+        }
+
+        public async Task SendAsync(Gener)
+        {
+
+        }
+
+
+    }
+}
